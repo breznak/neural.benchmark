d407ecd8c97287804f996d8f17e70420e7b29e62
wattik <najmanm@gmail.com>
2015-11-15 15:35:30 +0100

    New tool to plot datasets

---
+++ data/generatingScripts/plotDataset.m
@@ -0,0 +1,32 @@
+function plotDataset(data, name)
+    time = data(:,1);
+    func = data(:,2);
+    fazeState = data(:,3);
+    anomaly = data(:,4);
+    
+    if nargin < 2
+        name = 'Function f(x)';
+    end
+    
+  
+    a1 = subplot(3,1,1);
+        plot(time, func, 'b')
+    
+    a2 = subplot(3,1,2);      
+        plot(time,  fazeState, '--r');
+    
+    a3 = subplot(3,1,3);
+        plot(time, anomaly, 'o');
+    
+    title(a1, name);
+    xlabel(a1, 'Time [s]');
+    ylabel(a1, 'Function [-]');
+    title(a2, strcat(name, ' - faze state'));
+    xlabel(a2, 'Time [s]');
+    ylabel(a2, 'Faze state [-]');
+    title(a3, strcat(name, ' - Anomaly'));
+    xlabel(a3, 'Time [s]');
+    ylabel(a3, 'Anomaly (1/0)');
+    linkaxes([a1,a2,a3],'x');
+
+end
