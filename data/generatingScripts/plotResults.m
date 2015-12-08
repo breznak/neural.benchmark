37fcf25d6438f49f48448f9cb225c0b6d97b4d15
wattik <najmanm@gmail.com>
2015-12-08 18:11:33 +0100

    We can plot results now

---
+++ data/generatingScripts/plotResults.m
@@ -0,0 +1,37 @@
+function plotResults(time, func, anomaly, anomaly_likelihood, has_anomaly, name)
+    
+    if nargin < 6
+        name = 'Results for function f(x)';
+    end
+    
+  
+    a1 = subplot(3,1,1);
+        plot(time, func, 'b'); hold on
+        plot(time(has_anomaly==1), func(has_anomaly==1), 'r.');
+        
+    a2 = subplot(3,1,2);      
+        plot(time,  anomaly, 'g'); hold on;
+        plot(time, has_anomaly, 'o'); 
+    
+    a3 = subplot(3,1,3);
+        plot(time, anomaly_likelihood, 'r');
+    
+    title(a1, name, 'FontSize', 14);
+    xlabel(a1, 'Time [s]', 'FontSize', 12);
+    ylabel(a1, 'Function [-]', 'FontSize', 12);
+
+    title(a2, strcat(name, ' - Raw anomaly score'), 'FontSize', 14);
+    xlabel(a2, 'Time [s]', 'FontSize', 12);
+    ylabel(a2, 'Row anomaly [-]', 'FontSize', 12);
+
+    title(a3, strcat(name, ' - Anomaly likelihood'), 'FontSize', 14);
+    xlabel(a3, 'Time [s]', 'FontSize', 12);
+    ylabel(a3, 'Anomaly likelihood [-]' 'FontSize', 12);
+    
+    linkaxes([a1,a2,a3],'x');
+
+    set(a1, 'LineWidth', 3);
+    set(a2, 'LineWidth', 3);
+    set(a3, 'LineWidth', 3);
+
+end
