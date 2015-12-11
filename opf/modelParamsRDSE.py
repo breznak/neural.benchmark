fd5902d152343be363d75ee6b8b69b9c9a2d8e5f
wattik <najmanm@gmail.com>
2015-12-11 14:37:20 +0100

    New RDSE setting

--- opf/modelParamsRDSE.py
+++ opf/modelParamsRDSE.py
@@ -9,7 +9,7 @@ MODEL_PARAMS['modelParams']['sensorParams']['encoders'] = {
                     'fieldname': u'function',
                     'n': 1000,
                     'name': u'function',
-                    'resolution': 0.005,
+                    'resolution': 0.05,
                     'type': 'RandomDistributedScalarEncoder',
                     'w': 21,
                     'offset': 0,
